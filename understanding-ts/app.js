a7ab58fdb4f45cd7819fcceb4ef313589c6b6605
david-adds <55636118+david-adds@users.noreply.github.com>
2023-03-27 08:20:14 -0300

    typescript: arrays types

--- understanding-ts/app.js
+++ understanding-ts/app.js
@@ -5,7 +5,14 @@
 // } = {
 var person = {
     name: 'David',
-    lastName: 'Souza'
+    lastName: 'Souza',
+    hobbies: ['hiking', 'chess']
 };
+var favoriteFood;
+favoriteFood = ['lasagna', 'barbecue', 'pizza', 'burger'];
 console.log(person);
 console.log(person.lastName);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- understanding-ts/app.ts
+++ understanding-ts/app.ts
@@ -6,8 +6,16 @@
 
 const person ={
     name: 'David',
-    lastName: 'Souza'
+    lastName: 'Souza',
+    hobbies: ['hiking', 'chess']
 };
+
+let favoriteFood: string[];
+favoriteFood = ['lasagna', 'barbecue', 'pizza', 'burger'];
 
 console.log(person);
 console.log(person.lastName);
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+}
