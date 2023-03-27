3fa06d353b0ccc1ff0562f4d89d1de601cff476f
david-adds <55636118+david-adds@users.noreply.github.com>
2023-03-27 07:36:32 -0300

    typescript: using types

--- understanding-ts/app.js
+++ understanding-ts/app.js
@@ -1,1 +1,13 @@
-console.log('Your code goes here...');
+function add(n1, n2, showResult, msg) {
+    if (showResult) {
+        console.log("".concat(msg, " ").concat(n1 + n2));
+    }
+    else {
+        return n1 + n2;
+    }
+}
+var number1 = 5;
+var number2 = 2.8;
+var printResult = true;
+var message = 'Result is: ';
+add(number1, number2, printResult, message);
--- understanding-ts/app.ts
+++ understanding-ts/app.ts
@@ -1,1 +1,15 @@
-console.log('Your code goes here...');
+function add (n1: number, n2: number, showResult: boolean, msg: string){
+    if (showResult){
+        console.log(`${msg} ${n1 + n2}`);
+    }else{
+        return n1 + n2;
+    }
+}
+
+const number1 =  5;
+const number2 = 2.8;
+const printResult = true;
+const message = 'Result is: '
+
+add(number1, number2, printResult, message);
+
