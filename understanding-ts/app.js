989546789dd2938b3dbde354b31fb4c40e38b19a
david-adds <55636118+david-adds@users.noreply.github.com>
2023-03-27 22:09:04 -0300

    typescript: enums type

--- understanding-ts/app.js
+++ understanding-ts/app.js
@@ -1,10 +1,28 @@
 /*============ Object Type =============*/
+// const person: {
+//     name: string;
+//     lastName: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string] //tupple type
+// } = 
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+// enum Role {ADMIN, READ_ONLY, AUTHOR};
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 7] = "ADMIN";
+    Role[Role["READ_ONLY"] = 8] = "READ_ONLY";
+    Role["AUTHOR"] = "AUTHOR";
+})(Role || (Role = {}));
+;
 var person = {
     name: 'Nathalie',
     lastName: 'Souza',
     age: 32,
     hobbies: ['hiking', 'chess', 'crochet'],
-    role: [2, 'author']
+    role: Role.AUTHOR
 };
 var favoriteFood;
 favoriteFood = ['lasagna', 'barbecue', 'pizza', 'burger'];
@@ -14,5 +32,6 @@ for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
 }
-person.role.push('admin');
-console.log(person.role);
+if (person.role === Role.AUTHOR) {
+    console.log('is author');
+}
--- understanding-ts/app.ts
+++ understanding-ts/app.ts
@@ -1,16 +1,25 @@
 /*============ Object Type =============*/
-const person: {
-    name: string;
-    lastName: string;
-    age: number;
-    hobbies: string[];
-    role: [number, string] //tupple type
-} = {
+// const person: {
+//     name: string;
+//     lastName: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string] //tupple type
+// } = 
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+// enum Role {ADMIN, READ_ONLY, AUTHOR};
+enum Role {ADMIN = 7, READ_ONLY, AUTHOR='AUTHOR'};
+
+const person = {
     name: 'Nathalie',
     lastName: 'Souza',
     age: 32,
     hobbies: ['hiking', 'chess', 'crochet'],
-    role: [2, 'author']
+    role: Role.AUTHOR
 };
 
 let favoriteFood: string[];
@@ -22,5 +31,6 @@ console.log(person.lastName);
 for (const hobby of person.hobbies) {
     console.log(hobby.toUpperCase());
 }
-person.role.push('admin');
-console.log(person.role);
+if (person.role === Role.AUTHOR) {
+    console.log('is author')
+}
