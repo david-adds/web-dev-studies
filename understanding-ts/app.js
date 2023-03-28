788221fffd6fc0b9c302c8872263d110792422dc
david-adds <55636118+david-adds@users.noreply.github.com>
2023-03-28 08:33:38 -0300

    The "never" Type

--- understanding-ts/app.js
+++ understanding-ts/app.js
@@ -1,3 +1,12 @@
 var userInput;
+var userName;
 userInput = 7;
-userInput = 'Yes';
+userInput = 'John Wick';
+if (typeof userInput === 'string') {
+    userName = userInput;
+}
+function generateError(msg, code) {
+    throw { message: msg, errorCode: code };
+}
+var result = generateError('An error ocurred', 500);
+console.log(result);
--- understanding-ts/app.ts
+++ understanding-ts/app.ts
@@ -6,3 +6,11 @@ userInput = 'John Wick';
 if (typeof userInput === 'string') {
     userName = userInput;
 }
+
+function generateError(msg: string, code: number): never {
+    throw {message: msg, errorCode: code};
+    // while (true) {}
+}
+
+generateError('An error ocurred', 500);
+
