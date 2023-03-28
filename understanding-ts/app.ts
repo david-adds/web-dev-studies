14a92591bb7e9c30433cc09175cd638f23a94385
david-adds <55636118+david-adds@users.noreply.github.com>
2023-03-28 08:13:51 -0300

    Function Types & Callbacks

--- understanding-ts/app.ts
+++ understanding-ts/app.ts
@@ -4,7 +4,12 @@ function add(n1: number, n2: number) {
 
 function printResult(num: number): void {
     console.log('Result: ' + num );
-};
+}
+
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+    const result = n1 + n2;
+    cb(result);
+}
 
 printResult(add(5 , 65));
 
@@ -13,3 +18,8 @@ let combineValues: (a: number, b: number) => number;
 combineValues = add;
 
 console.log(combineValues(8, 8));
+
+addAndHandle(10, 20, (result) => {
+        console.log(result);
+    }
+);
