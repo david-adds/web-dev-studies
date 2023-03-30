3a9f552e5992878ae11f180b175f069e0875cc3a
david-adds <55636118+david-adds@users.noreply.github.com>
2023-03-30 00:27:19 -0300

    never Type

---
+++ understanding-ts/other_lectures/never-type.ts
@@ -0,0 +1,15 @@
+let  userInput: unknown;
+let userName: string;
+
+userInput = 8;
+userInput = 'John Wick';
+if (typeof userInput === 'string') {
+    userName = userInput;
+}
+
+function generateError(msg: string, code: number): never {
+    throw {message: msg, errorCode: code};
+    // while (true) {}
+}
+
+generateError('An error ocurred', 500);
