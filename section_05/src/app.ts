eab3907eacc6e7754c0d7168640e3c35f946a115
david-adds <55636118+david-adds@users.noreply.github.com>
2023-04-02 13:54:25 -0300

    "private" and "public" Access Modifiers

--- section_05/src/app.ts
+++ section_05/src/app.ts
@@ -1,18 +1,29 @@
 class Department {
     name: string;
+    private employees: string[] = [];
 
     constructor(n: string) {
         this.name = n;
     }  
 
     describe (this: Department) {
-        console.log(`Department: ${this.name}`)
+        console.log(`Department: ${this.name}`);
+    }
+
+    addEmployee(employee: string) {
+        this.employees.push(employee);
+    }
+
+    printEmployeeInformation () {
+        console.log(this.employees.length);
+        console.log(this.employees);
     }
 }
 
 const accounting = new Department('Accounting');
-// console.log(accounting);
-accounting.describe();
+accounting.addEmployee('John Doe');
+accounting.addEmployee('Jane Doe');
+// accounting.employees[2]='Anna';
 
-const accountingCopy = {name: 'Dummy', describe: accounting.describe};
-accountingCopy.describe();
+accounting.describe();
+accounting.printEmployeeInformation();
