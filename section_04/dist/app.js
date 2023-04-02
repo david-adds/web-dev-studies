86ff162d5c8f43e3a8d113ab96daef0565e4413d
david-adds <55636118+david-adds@users.noreply.github.com>
2023-04-02 01:24:10 -0300

    Arrow Functions

---
+++ section_04/dist/app.js
@@ -0,0 +1,7 @@
+"use strict";
+const add = (a, b) => a + b;
+console.log(add(2, 5));
+const printOutput = (output) => {
+    console.log(output);
+};
+printOutput(add(4, 6));
--- section_04/src/app.ts
+++ section_04/src/app.ts
@@ -1,1 +1,9 @@
-console.log('Hello world')
+const add = (a:number, b:number) => a + b;
+
+console.log(add(2,5));
+
+const printOutput = (output: string | number) => {
+    console.log(output);
+}
+
+printOutput(add(4,6));
